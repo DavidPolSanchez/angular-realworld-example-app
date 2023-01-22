ecfdd2441a4776ead59dfaee23348f4b9f82f4ac
dsanchez <david.martos@amaris.com>
2023-01-22 20:37:41 +0100

    add env variables on config file

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,16 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   viewportHeight:1080,
   viewportWidth:1920,
+  env:{
+    username:'davidpolsanchezmartos@gmail.com',
+    password:'caracoles',
+    baseUrl:'https://api.realworld.io'
+  },
+  retries:1,
   e2e: {
+    setupNodeEvents(on, config) {
+      // e2e testing node events setup code
+    },
     baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'],
