ef38597ea9ddad04c6d6113ce56a525eaf70fcb4
dsanchez <david.martos@amaris.com>
2023-01-22 20:27:35 +0100

    fix text on logout btn

--- cypress/e2e/secondTest.specs.js
+++ cypress/e2e/secondTest.specs.js
@@ -5,7 +5,7 @@ describe('Test log out',()=>{
 
     it('verify can log out corectly',()=>{
         cy.contains('Settings').click()
-        cy.contains('Or click here to log out').click()
+        cy.contains('Or click here to logout').click()
         cy.get('.navbar-nav').should('contain','Sign up')
     })
 })
