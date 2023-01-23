50fa2f961100bafa2926c704bf920e9d1d095632
dsanchez <david.martos@amaris.com>
2023-01-23 16:04:59 +0100

    add projectId for cloud cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'tu31v9',
   viewportHeight:1080,
   viewportWidth:1920,
   reporter:"cypress-multi-reporters",
